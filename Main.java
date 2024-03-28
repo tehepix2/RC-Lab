99bdb4b78e5d172b21fc445d039b602657bd4d06
tehepix2 <daniel12landa@gmail.com>
2024-03-27 23:08:17 -0400

    finished lab

--- Main.java
+++ Main.java
@@ -5,48 +5,78 @@ class Main {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         lib.clearConsole();
         Scanner input = new Scanner(System.in);
-
+        
         File para = new File("params.txt");
         para.createNewFile();
         Scanner paraR = new Scanner(para);
         FileWriter paraW = new FileWriter(para, true);
+        
 
         File rc = new File("rc.txt");
         rc.createNewFile();
         Scanner rcR = new Scanner(rc);
-        FileWriter rcW = new FileWriter("rc.txt");
+        FileWriter rcW = new FileWriter("rc.txt", true);
 
-        
-        System.out.print("V = ");
+        File[] fileList = {para, rc};
+
+        System.out.print("Initial Time: ");
+        String t = input.nextLine();
+        paraW.write(t + "\n");
+        lib.clearConsole();
+
+        System.out.print("Final Time: ");
+        String t2 = input.nextLine();
+        paraW.write(t2 + "\n");
+        lib.clearConsole();
+
+        System.out.print("V: ");
         String v = input.nextLine();
         paraW.write(v + "\n");
         lib.clearConsole();
-        
 
-        System.out.print("R = ");
+        System.out.print("R: ");
         String r = input.nextLine();
         paraW.write(r + "\n");
         lib.clearConsole();
 
-        System.out.print("C = ");
+        System.out.print("C: ");
         String c = input.nextLine();
-        paraW.write(c + "\n");
-        lib.clearConsole();
-
-        System.out.print("T = ");
-        String t = input.nextLine();
-        paraW.write(t);
+        paraW.write(c);
         lib.clearConsole();
         paraW.close();
 
-        System.out.println(equation(Double.valueOf(paraR.nextLine()), Double.valueOf(paraR.nextLine()), Double.valueOf(paraR.nextLine()), Integer.valueOf(paraR.nextLine())));
-       
+        int par1 = Integer.valueOf(paraR.nextLine());
+        int par2 = Integer.valueOf(paraR.nextLine());
+        for (int i = par1; i < par2 + 1; i++) {
+            
+            paraR = new Scanner(para);
+            paraR.nextLine();
+            paraR.nextLine();
+        
+            rcW.write("Time: " + i + " -- " + Double.toString(equation(Double.valueOf(paraR.nextLine()), Double.valueOf(paraR.nextLine()), Double.valueOf(paraR.nextLine()), i)) + "\n");
+           
+        }
+        
+        rcW.close();
+        while (rcR.hasNextLine()) {
+            System.out.println(rcR.nextLine());
+        }
+        
+        
         paraR.close();
+        
+        paraW.close();
         rcR.close();
         rcW.close();
+        
         input.close();
-        para.delete();
-        rc.delete();
+        
+        System.gc(); // fix for the params file not deleting found online
+        for(File f : fileList){
+            f.delete();
+        }
+        
+       
         
 
 
